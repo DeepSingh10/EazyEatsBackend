233fabedabc069ecd83eb22d3b2a2b772a66266f
Deep <deepsinghyadav933@gmail.com>
2024-02-15 10:40:47 +0530

    commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
-const port = 5000
+const port = process.env.port || 5000
 const mongoDB = require('./db')
 mongoDB();
 
