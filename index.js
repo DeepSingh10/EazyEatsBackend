c86d123d56cb6dd751fb6a41bb6eb9fc3a317da7
Deep <deepsinghyadav933@gmail.com>
2024-02-15 11:53:55 +0530

    commit

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use((req,res,next)=>{
     // res.header(
     //     "Access-Control-Allow-Origin"
     // );
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET", true);
     next();
 })
 
